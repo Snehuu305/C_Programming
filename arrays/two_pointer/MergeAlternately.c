aede7c5c216127c122ad73e4a54c6f15a060b4a9
SnehaBorgaon <snehaborgaon26@gmail.com>
2026-07-17 22:10:51 +0530

    Add files via upload

---
+++ arrays/two_pointer/MergeAlternately.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+
+char* mergeAlternately(char* word1, char* word2) {
+    
+    int len1 = strlen(word1);
+    int len2 = strlen(word2);
+    
+    char* result = (char*)malloc(len1 + len2 + 1);
+    
+    int i = 0;
+    int j = 0;
+    int k = 0;  
+
+    while (i < len1 && j < len2) {
+        result[k++] = word1[i++];
+        result[k++] = word2[j++];
+    }
+
+    while (i < len1) {
+        result[k++] = word1[i++];
+    }
+
+    while (j < len2) {
+        result[k++] = word2[j++];
+    }
+
+    result[k] = '\0';  
+
+    return result;
+}
