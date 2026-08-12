c23904974e263e35255d73e1bf12b4bbdff18c53
SnehaBorgaon <snehaborgaon26@gmail.com>
2026-08-12 22:18:05 +0530

    Add files via upload

---
+++ arrays/two_pointer/Trap.c
@@ -0,0 +1,25 @@
+int trap(int* height, int heightSize) {
+    int left = 0, right = heightSize - 1;
+    int leftMax = 0, rightMax = 0;
+    int totalWater = 0;
+
+    while (left < right) {
+        if (height[left] < height[right]) {
+            if (height[left] >= leftMax) {
+                leftMax = height[left];
+            } else {
+                totalWater += leftMax - height[left];
+            }
+            left++;
+        } else {
+            if (height[right] >= rightMax) {
+                rightMax = height[right];
+            } else {
+                totalWater += rightMax - height[right];
+            }
+            right--;
+        }
+    }
+
+    return totalWater;
+}
