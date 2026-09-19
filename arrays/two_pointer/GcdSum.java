c39b1aca9942bf54102e5f6c1bf507fd20dbab41
SnehaBorgaon <snehaborgaon26@gmail.com>
2026-09-19 22:39:20 +0530

    Format GcdSum.java for consistency

--- arrays/two_pointer/GcdSum.java
+++ arrays/two_pointer/GcdSum.java
@@ -1,9 +1,11 @@
 class Solution {
-    public long gcdSum(int[] nums) {
+    public long gcdSum(int[] nums) 
+    {
         int[] prefixGcd = new int[nums.length];
         int max = -1;
 
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++)
+        {
             max = Math.max(max, nums[i]);
             prefixGcd[i] = gcd(max, nums[i]);
         }
