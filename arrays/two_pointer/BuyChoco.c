b1b05672124bef0ff85601ca441e20e50919fffd
SnehaBorgaon <snehaborgaon26@gmail.com>
2026-09-19 22:38:18 +0530

    Update BuyChoco.c

--- arrays/two_pointer/BuyChoco.c
+++ arrays/two_pointer/BuyChoco.c
@@ -3,10 +3,10 @@ int cmp(const void* a, const void* b) {
 }
 
 int buyChoco(int* prices, int pricesSize, int money) {
-    qsort(prices, pricesSize, sizeof(int), cmp);  // step 1: sort so cheapest 2 are at index 0 and 1
-    int cost = prices[0] + prices[1];             // step 2: cost of cheapest pair
+    qsort(prices, pricesSize, sizeof(int), cmp);  
+    int cost = prices[0] + prices[1];            
     
-    if (cost <= money) {                          // step 3: check affordability
+    if (cost <= money) {                          
         return money - cost;
     } else {
         return money;
